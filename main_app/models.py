24d6d6821fd7d6ebc2e45b12c3011d216e5b77be
RareTC <trevorjcampbell@aol.com>
2023-03-03 15:03:45 -0800

    Auth added

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,5 @@
 from django.db import models
-
-# Create your models here.
+from django.urls import reverse
+from datetime import date
+# Import the User
+from django.contrib.auth.models import User
