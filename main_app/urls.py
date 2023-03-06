ae9a04c3f983ba211b56346dee52860e73b61f62
coffeemug17 <mitulpandey2@gmail.com>
2023-03-06 11:11:44 -0800

    Forms added

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('projects/', views.projects_index, name='index'),
     path('accounts/signup/', views.signup, name='signup'),
+    path('projects/<int:project_id>/', views.projects_detail, name='detail'),
+    path('projects/create/', views.ProjectCreate.as_view(), name='projects_create'),
+    path('projects/<int:pk>/update/', views.ProjectUpdate.as_view(), name='projects_update'),
+    path('projects/<int:pk>/delete/', views.ProjectDelete.as_view(), name='projects_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Project
 
 # Create your views here.
+class ProjectCreate(LoginRequiredMixin, CreateView):
+  model = Project
+  fields = '__all__'
+
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
+
+
 def home(request):
     return render(request, 'home.html')
 
@@ -34,3 +46,4 @@ def signup(request):
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
+
