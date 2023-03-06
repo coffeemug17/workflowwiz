1a739433ec13eda3fef44edc6a3dfbd0f4b5664f
RareTC <trevorjcampbell@aol.com>
2023-03-06 09:49:22 -0800

    project model

--- main_app/models.py
+++ main_app/models.py
@@ -3,3 +3,19 @@ from django.urls import reverse
 from datetime import date
 # Import the User
 from django.contrib.auth.models import User
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    completion = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'({self.name} {self.id})'
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'project_id': self.id})
+    
+    
+
+    
